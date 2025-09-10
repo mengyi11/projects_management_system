b5dc34c1924f0c691bfea07c43d621dc2eceeca3
mengyi11 <mxu027@e.ntu.edu.sg>
2025-09-10 12:51:54 +0800

    Add Icon from @mui/icons-material and save it file

---
+++ src/lib/iconMap.js
@@ -0,0 +1,15 @@
+import PersonIcon from '@mui/icons-material/Person';
+import EventIcon from '@mui/icons-material/Event';
+import WorkIcon from '@mui/icons-material/Work';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import EmailIcon from '@mui/icons-material/Email';
+
+const iconMap = {
+  Person: PersonIcon,
+  Event: EventIcon,
+  Work: WorkIcon,
+  BarChart: BarChartIcon,
+  Email: EmailIcon,
+};
+
+export default iconMap;
