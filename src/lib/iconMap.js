6e9ab6fdd33aa5b149c72cd7fdcf320da302c6a7
mengyi11 <mxu027@e.ntu.edu.sg>
2025-09-12 01:26:52 +0800

    Add Grade&Course&Enter Icon

--- src/lib/iconMap.js
+++ src/lib/iconMap.js
@@ -2,14 +2,19 @@ import PersonIcon from '@mui/icons-material/Person';
 import EventIcon from '@mui/icons-material/Event';
 import WorkIcon from '@mui/icons-material/Work';
 import BarChartIcon from '@mui/icons-material/BarChart';
+import GradingOutlinedIcon from '@mui/icons-material/GradingOutlined';
 import EmailIcon from '@mui/icons-material/Email';
+import TopicIcon from '@mui/icons-material/Topic';
+import ArrowForwardOutlinedIcon from '@mui/icons-material/ArrowForwardOutlined';
 
 const iconMap = {
   Person: PersonIcon,
   Event: EventIcon,
   Work: WorkIcon,
-  BarChart: BarChartIcon,
+  Grade: GradingOutlinedIcon,
   Email: EmailIcon,
+  Course: TopicIcon,
+  Enter: ArrowForwardOutlinedIcon,
 };
 
 export default iconMap;
