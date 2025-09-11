d46923a0f7aace21b6f5336fc854ac69303750fe
mengyi11 <mxu027@e.ntu.edu.sg>
2025-09-12 01:33:13 +0800

    Unify admin layout

---
+++ src/components/AdminLayout.jsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { ThemeProvider, Container, Box } from "@mui/material";
+import SideNavBar from "@/components/SideNavBar";
+import NavBreadcrumbs from "@/components/NavBreadcrumbs";
+import customTheme from '@/styles/customTheme.js';
+
+export default function AdminLayout({ children, pathArr, userRole }) {
+  return (
+    <div style={{ display: "flex", minHeight: "100vh" }}>
+        <ThemeProvider theme={customTheme}>
+            <SideNavBar role={userRole} />
+            <Container maxWidth="xl">
+                <NavBreadcrumbs path={pathArr} />
+                <Box sx={{ pl: 2, mt: 3 }}>
+                {children}
+                </Box>
+            </Container>
+       </ThemeProvider>
+    </div>
+  );
+}
