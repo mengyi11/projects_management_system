d85a3113b47575230f5878ef993ff627fe006308
mengyi11 <mxu027@e.ntu.edu.sg>
2025-09-12 02:49:47 +0800

    Add new Icon

--- src/lib/iconMap.js
+++ src/lib/iconMap.js
@@ -1,7 +1,7 @@
 import PersonIcon from '@mui/icons-material/Person';
 import EventOutlinedIcon from '@mui/icons-material/EventOutlined';
 import WorkIcon from '@mui/icons-material/Work';
-import BarChartIcon from '@mui/icons-material/BarChart';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import GradingOutlinedIcon from '@mui/icons-material/GradingOutlined';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import TopicIcon from '@mui/icons-material/Topic';
@@ -9,9 +9,11 @@ import ArrowForwardOutlinedIcon from '@mui/icons-material/ArrowForwardOutlined';
 import NoteAltOutlinedIcon from '@mui/icons-material/NoteAltOutlined';
 import StickyNote2OutlinedIcon from '@mui/icons-material/StickyNote2Outlined';
 import PersonAddAltOutlinedIcon from '@mui/icons-material/PersonAddAltOutlined';
+import FolderSpecialOutlinedIcon from '@mui/icons-material/FolderSpecialOutlined';
 
 const iconMap = {
   Person: PersonAddAltOutlinedIcon,
+  Person2: PersonOutlineOutlinedIcon,
   Event: EventOutlinedIcon,
   Work: WorkIcon,
   Project: StickyNote2OutlinedIcon,
@@ -20,6 +22,7 @@ const iconMap = {
   Course: TopicIcon,
   Enter: ArrowForwardOutlinedIcon,
   Proposal: NoteAltOutlinedIcon,
+  Registration: FolderSpecialOutlinedIcon,
 };
 
 export default iconMap;
