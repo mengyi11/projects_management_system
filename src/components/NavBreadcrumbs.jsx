deca840b0dea9502bf1e322078a10057159aded2
mengyi11 <mxu027@e.ntu.edu.sg>
2025-09-12 17:29:43 +0800

    Update breadcurmbs's Link component to MUI

--- src/components/NavBreadcrumbs.jsx
+++ src/components/NavBreadcrumbs.jsx
@@ -1,34 +1,30 @@
 import React from 'react';
-import { Box, Breadcrumbs, Typography,Divider } from '@mui/material';
+import { Box, Breadcrumbs, Typography, Divider, Link } from '@mui/material';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import HomeIcon from "@mui/icons-material/Home";
-import Link from 'next/link';
 
-const NavBreadcrumbs = ({  path }) => {
+const NavBreadcrumbs = ({ path }) => {
     return (
-            <Box display="flex" alignItems="center" pl={1} mt={3} pb={2}>
-                <HomeIcon sx={{ mr: 1, color: "text.secondary" }} />
-                <Divider orientation="vertical" flexItem />
-                <Breadcrumbs
-                sx={{ ml: 3}} 
-                    separator={<NavigateNextIcon fontSize="small"  />}
-                >
-                    {path.map((item, index) => (
+        <Box display="flex" alignItems="center" pl={1} mt={3} pb={2}>
+            <HomeIcon sx={{ mr: 1, color: "text.secondary" }} />
+            <Divider orientation="vertical" flexItem />
+            <Breadcrumbs
+                sx={{ ml: 3 }}
+                separator={<NavigateNextIcon fontSize="small" />}
+            >
+                {path.map((item, index) => (
                     <Link
                         key={index}
-                        underline="hover"
-                        color="inherit"
-                        href="/student"
-                        sx={{ display: "flex", alignItems: "center" }}
+                        href={item.toLowerCase()}
                     >
-                        <Typography fontSize="0.8rem">{item}</Typography>
+                        <Typography variant='nav'>{item}</Typography>
                     </Link>
-                    ))}
-                </Breadcrumbs>
-            </Box>
-       
+                ))}
+            </Breadcrumbs>
+        </Box>
 
-        
+
+
     )
 }
 
