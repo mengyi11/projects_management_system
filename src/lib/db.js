f09a359453a97d5975a17181b7f1d7eeba0995ed
mengyi11 <mxu027@e.ntu.edu.sg>
2025-09-15 06:25:04 +0800

    update database config

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,9 +1,12 @@
 import mysql from 'mysql2/promise';
 
-export const db = mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,       
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE, 
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
+
+export default pool;
