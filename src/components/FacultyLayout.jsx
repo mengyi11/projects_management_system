fbdc28e8c9b56f0810c3a8a3921a59db486e2b16
mengyi11 <mxu027@e.ntu.edu.sg>
2025-12-05 11:13:57 +0800

    update the faculty‘s layout

--- src/components/FacultyLayout.jsx
+++ src/components/FacultyLayout.jsx
@@ -5,18 +5,16 @@ import SideNavBar from "@/components/SideNavBar";
 import NavBreadcrumbs from "@/components/NavBreadcrumbs";
 import customTheme from '@/styles/customTheme.js';
 
-export default function FacultyLayout({ children, pathArr, userRole }) {
+export default function FacultyLayout({ children, pathArr }) {
   return (
     <div style={{ display: "flex", minHeight: "100vh" }}>
-        <ThemeProvider theme={customTheme}>
-            <SideNavBar role={userRole} />
-            <Container maxWidth="xl">
-                <NavBreadcrumbs path={pathArr} />
-                <Box sx={{ pl: 2, mt: 3 }}>
-                {children}
-                </Box>
-            </Container>
-       </ThemeProvider>
+      <SideNavBar role='Faculty' />
+      <Container maxWidth="xl">
+        <NavBreadcrumbs path={pathArr} />
+        <Box sx={{ pl: 2, mt: 3 }}>
+          {children}
+        </Box>
+      </Container>
     </div>
   );
 }
