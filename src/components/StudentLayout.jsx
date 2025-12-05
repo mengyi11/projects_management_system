51f98a43c22c92b81655bef37a32446c5b68520e
mengyi11 <mxu027@e.ntu.edu.sg>
2025-12-05 11:14:21 +0800

    implement the student‘s layout

---
+++ src/components/StudentLayout.jsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { ThemeProvider, Container, Box } from "@mui/material";
+import SideNavBar from "@/components/SideNavBar";
+import NavBreadcrumbs from "@/components/NavBreadcrumbs";
+import customTheme from '@/styles/customTheme.js';
+
+export default function StudentLayout({ children, pathArr }) {
+  return (
+    <div style={{ display: "flex", minHeight: "100vh" }}>
+      <SideNavBar role='Student' />
+      <Container maxWidth="xl">
+        <NavBreadcrumbs path={pathArr} />
+        <Box sx={{ pl: 2, mt: 3 }}>
+          {children}
+        </Box>
+      </Container>
+    </div>
+  );
+}
